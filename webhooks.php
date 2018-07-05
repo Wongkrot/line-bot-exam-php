9de45f40db82300d269a67f7d005f4d30f48e435
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-05 12:24:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,9 +17,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['source']['userId'];
-			$text = $event['source']['display-name'];
+			$text = $event['source']['userId'];
 			
+			//$text = $event['source']['display-name'];
 			// $profile = $res->getJSONDecodedBody();
    			// $displayName = $profile['displayName'];
    			// $statusMessage = $profile['statusMessage'];
