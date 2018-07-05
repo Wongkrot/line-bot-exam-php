1b9ba69a359d5271f15d2e41fbc1c73ab797ee0f
Wongkrot <32104088+Wongkrot@users.noreply.github.com>
2018-07-05 12:23:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			//$text = $event['source']['userId'];
-			$text = $event['source']['displayName'];
+			$text = $event['source']['display-name'];
 			
 			// $profile = $res->getJSONDecodedBody();
    			// $displayName = $profile['displayName'];
